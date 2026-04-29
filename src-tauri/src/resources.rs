c468a69d650876e91ba5cc94f2b76b6b87edbb29
shellrow <shellrow@foctal.com>
2026-04-29 12:27:32 +0900

    refactor: load port presets from resource files

--- src-tauri/src/resources.rs
+++ src-tauri/src/resources.rs
@@ -4,5 +4,9 @@ pub const SERVICE_PROBES_JSON: &str = include_str!("../resources/nd-service-prob
 pub const PORT_PROBES_JSON: &str = include_str!("../resources/nd-port-probes.json");
 /// Top 100 ports preset
 pub const TOP_100_PORTS_JSON: &str = include_str!("../resources/nd-top-100-ports.json");
+/// Top 1000 ports preset
+pub const TOP_1000_PORTS_JSON: &str = include_str!("../resources/nd-top-1000-ports.json");
+/// Well-known ports preset
+pub const WELLKNOWN_PORTS_JSON: &str = include_str!("../resources/nd-wellknown-ports.json");
 /// TLS OID mappings
 pub const TLS_OID_MAP_JSON: &str = include_str!("../resources/nd-tls-oid-map.json");
